52806b43e1e44976f4664005a1712af13c2628a1
dariopa <dariopa@ethz.ch>
2017-05-18 15:21:59 +0200

    Move the monster

---
+++ Assets/_Scripts/MoveMonster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Move_Monster : MonoBehaviour {
+	public float monster_speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0, 0, 1*monster_speed) * Time.deltaTime);
+	}
+}
