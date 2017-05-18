f2505ee2740c428f29b980db2f6f3dc1964553c2
Dario Panzuto <Dario Panzuto>
2017-05-19 00:48:22 +0200

    Code Snippets
    I implemented a reduced game of ours and wrote some code which we will be hopefully able to use tomorrow - have a look at it! :)

---
+++ VR_GAME_2017_Code_Snippets/Assets/_Scripts/DestroyMonster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class DestroyMonster : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Monster"))
+            {
+                Destroy(other.gameObject);
+                //other.gameObject.SetActive(false);
+            }
+        }
+}
---
+++ VR_GAME_2017_Code_Snippets/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// [System.Serializable] makes it possible, to cache the variables in unity. 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    
+
+    public float speed;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float FireRate = 0.5F;
+
+    private Rigidbody rb;
+
+    private float NextFire = 0.0F;
+
+
+    private void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+    }
+    // Fire the shots!
+    private void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > NextFire)
+        {
+            NextFire = Time.time + FireRate;
+            // Instantiate(object, position, rotation);
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject; 
+        }        
+    }
+
+
+        private void FixedUpdate()
+    {
+        // Move the player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(-moveHorizontal, moveVertical, 0.0f) * speed;
+
+        // This vector clamps the player in the game field! 
+        rb.position = new Vector3 
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+                0.0f
+            );        
+    }
+}
