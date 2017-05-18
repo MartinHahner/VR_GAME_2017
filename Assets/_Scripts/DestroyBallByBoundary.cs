bf9bfb084ae7ef2763ae5fd7e978b1c16aebf6bb
Dario Panzuto <Dario Panzuto>
2017-05-18 23:08:14 +0200

    Boundary to delete Ball
    As soon as the Ball will hit the boundary around the playfield, it will be deleted during the game.

---
+++ Assets/_Scripts/DestroyBallByBoundary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBallByBoundary : MonoBehaviour {
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
