22299aeab79f63367400bca0734e4f31396d5613
corinthosis <guralnickds@gmail.com>
2017-05-18 17:24:33 +0200

    Ignore Collision
    Colliders necessary for controlling the cannon will not interact with each other

---
+++ Assets/_Scripts/IgnorePoleCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnorePoleCollision : MonoBehaviour {
+
+	public GameObject Cylinder_Vertical;
+	public GameObject Cylinder_Horizontal;//Collider actually attached to Cannon Plane
+
+	// Use this for initialization
+	void Start () {
+		Cylinder_Vertical = GameObject.FindWithTag ("VerticalPole");
+		Cylinder_Horizontal = GameObject.FindWithTag ("HorizontalPole");
+	
+		Physics.IgnoreCollision(Cylinder_Vertical.GetComponent<Collider>(), Cylinder_Horizontal.GetComponent<Collider>(), true);
+	}
+
+}
