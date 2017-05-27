c64af343316b09d2b5ffbe6b5f591f42a24eec76
Martin Hahner <martin.hahner@gmail.com>
2017-05-27 18:35:23 +0200

    canon behaves the same now in both worlds

---
+++ Assets/_Scripts/BoxesNotFalling.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxesNotFalling : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+    }
+}
