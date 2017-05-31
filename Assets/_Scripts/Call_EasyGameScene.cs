085d8794dadfc0035e8beb7ec120de4f8430e4ba
corinthosis <guralnickds@gmail.com>
2017-05-31 11:23:24 +0200

    easy and hard level :)

---
+++ Assets/_Scripts/Call_EasyGameScene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Call_EasyGameScene : MonoBehaviour {
+
+	SteamVR_Controller.Device deviceR;
+	public SteamVR_TrackedObject trackedObjR;
+	SteamVR_Controller.Device deviceL;
+	public SteamVR_TrackedObject trackedObjL;
+
+	void Start () {
+		GetComponent<SphereCollider> ().isTrigger = true;
+		deviceR = SteamVR_Controller.Input((int)trackedObjR.index);
+		deviceL = SteamVR_Controller.Input((int)trackedObjL.index);
+	}
+
+	void rumbleController()
+	{
+
+		StartCoroutine(LongVibration(1, 3999));
+
+	}
+
+	IEnumerator LongVibration(float length, float strength)
+	{
+		for (float i = 0; i < length; i += Time.deltaTime)
+		{
+			deviceR.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+			deviceL.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+			yield return null;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//void OnMouseDown(){
+		if (other.gameObject.tag == "controller")
+		{
+			rumbleController();
+		}
+		Application.LoadLevel ("Easy_CannonGame");
+	}
+}
